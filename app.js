9442a45eccf7119a4f67c16615906526c35ab032
Samikshya <samikshyaaryal5@gmail.com>
2019-11-11 18:13:05 +0200

    connecting to mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const mongoose = require ('mongoose'); 
 const app = express();
+require('dotenv/config');
+
+
+//Middlewares
+app.use('/posts' ,() => {
+    console.log('Midddlewares mesage');
+})
 
 //Routes 
 app.get('/', (req, res) =>  {
     res.send('Home');
 })
+// connecting mongoDb 
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {useNewUrlParser: true},
+    () => console.log('connected to ')
+);
+
 
 app.listen(3000); // listening to the server 
 
